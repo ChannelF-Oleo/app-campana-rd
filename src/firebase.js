7958b50325771f5ce7902cc878e8f75a376f88ce
ChannelF-Oleo <channelf.oleo@gmail.com>
2025-11-13 00:04:13 -0400

    feat: Prepare Workbox for large PDF by configuring max file size. Fixed all remaining ESLint warnings/errors.

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Al principio, con las otras importaciones de firebase
 import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "./firabaseconfig";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
 // TODO: Agrega la configuración de tu SDK de Firebase
 // La configuración de tu app web de Firebase
